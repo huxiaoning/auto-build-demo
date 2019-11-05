0a10b4a1dbcce124af52be32d2711ddcd2c772d2
huxiaoningsworld <huxiaoningsworld@sina.com>
2019-11-05 17:41:36 +0800

    docker auto build demo

---
+++ src/test/java/org/aidan/autobuilddemo/AutoBuildDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aidan.autobuilddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBuildDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
